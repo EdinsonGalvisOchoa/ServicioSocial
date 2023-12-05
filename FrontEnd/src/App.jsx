012fdf67360a395f03ac8fc23f09619ca237dade
Edinson <edinsongalvisochoa@gmail.com>
2023-12-04 19:47:20 -0500

    Commit FrontEnd Formularios publicos de inicio de sesion y registro

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,10 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-
+import AuthLayout from "./layouts/AuthLayout"
+import Login from "./paginas/Login"
+import Registrar from "./paginas/Registrar"
+import OlvidePassword from "./paginas/OlvidePassword"
+import NuevoPassword from "./paginas/NuevoPassword"
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta"
 function App() {
  
 
@@ -7,11 +12,13 @@ function App() {
     <BrowserRouter>
     <Routes>
 
-      <Route path="/">
-
+      <Route path="/" element={<AuthLayout />}>
+        <Route index element={<Login />} />
+        <Route path="registrar" element={<Registrar/>} />
+        <Route path="olvide-password" element= {<OlvidePassword/>} />
+        <Route path="olvide-password/:token" element= {<NuevoPassword/>} />
+        
       </Route>
-
-
     </Routes>
     </BrowserRouter>
   )
---
+++ FrontEnd/src/layouts/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+const AuthLayout = () => {
+  return (
+    <>
+    <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
+        <Outlet/>
+    </main> 
+    </>   
+  )
+}
+
+export default AuthLayout
