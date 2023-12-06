4f522b8991fdac42fd3f4e0d89673795fd9d77a0
Edinson <edinsongalvisochoa@gmail.com>
2023-12-05 21:37:03 -0500

    Validacion del Token para resetear el password

--- FrontEnd/src/paginas/NuevoPassword.jsx
+++ FrontEnd/src/paginas/NuevoPassword.jsx
@@ -1,27 +1,72 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Alertas from "../components/Alertas";
 
 const NuevoPassword = () => {
-    return (
-        <>
-          <h1 className="text-sky-600 font-black text-6xl capitalize">Reestablecer contraseña</h1>
-    
-          <form className="my-10 bg-white shadow rounded-lg p-10">
-            <div className="my-5">
-                <label className="uppercase text-gray-600 block text-xl font-bold" htmlfor="password">Nuevo Password</label>
-                <input id="password"type="password" placeholder="Escribe tu nuevo password" className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
-            </div>
-            <input type="submit" value="Guardar nuevo password" className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"/>
-          </form>
-          <nav className="lg:flex lg:justify-between">
+  const [tokenValido, setTokenValido] = useState(false);
+  const [alerta, setAlerta] = useState({});
 
-    
-    
-          </nav>
-    
-    
-        </>
-      );
-  
-}
+  const params = useParams();
+  const { token } = params;
 
-export default NuevoPassword
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        const { data } = await axios.get(
+          `${
+            import.meta.env.VITE_BACKEND_URL
+          }/api/usuarios/olvide-password/${token}`
+        );
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg:error.response.data.msg,
+          error:true
+        });
+      }
+    };
+    comprobarToken();
+  }, []); // le ponemos el arreglo de dependencias vacio para que se ejecute una sola vez
+
+  const {msg} = alerta
+
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Reestablecer contraseña
+      </h1>
+
+      {msg && <Alertas alerta = {alerta} />}
+
+      { tokenValido && (
+        <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+          >
+            Nuevo Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Escribe tu nuevo password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Guardar nuevo password"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+      
+      )
+      
+      }
+    </>
+  );
+};
+
+export default NuevoPassword;
