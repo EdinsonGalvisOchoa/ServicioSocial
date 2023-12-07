7861ae32eb81a7ab00b369e10f5861b4611fae28
Edinson <edinsongalvisochoa@gmail.com>
2023-12-06 20:55:02 -0500

    HandleSubmit en el login

--- FrontEnd/src/paginas/Login.jsx
+++ FrontEnd/src/paginas/Login.jsx
@@ -8,13 +8,21 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [alerta, setAlerta] = useState("");
 
+  const handleSubmit = async e => {
+
+    e.preventDefault();
+
+
+
+  }
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Inicia sesión{" "}
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={handleSubmit}>
         <div className="my-5">
           <label
             className="uppercase text-gray-600 block text-xl font-bold"
